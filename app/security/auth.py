57fa4f142710aac5e9128445bfd2f6aecd0efcf5
rubenabimalec <al19760767@ite.edu.mx>
2026-03-04 21:16:24 -0800

    Authentication module ready

---
+++ app/security/auth.py
@@ -0,0 +1,16 @@
+import pam
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+security = HTTPBasic()
+
+def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
+    p = pam.pam()
+    if p.authenticate(credentials.username, credentials.password):
+        return credentials.username
+    
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Usuario o contraseña inválidos",
+        headers={"WWW-Authenticate": "Basic"},
+    )
