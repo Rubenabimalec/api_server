ccfc7f0881aaa2dda43acea8f1b789390681371a
rubenabimalec <al19760767@ite.edu.mx>
2026-03-06 15:07:50 -0800

    modificacion liera

--- app/services/command_executor.py
+++ app/services/command_executor.py
@@ -2,23 +2,22 @@ import subprocess
 from fastapi import HTTPException
 from config.settings import ALLOWED_COMMANDS
 
-def execute_system_command(command_name, params):
+def execute_system_command(command, params):
 
-    if command_name not in ALLOWED_COMMANDS:
-        raise HTTPException(status_code=403, detail="Comando no autorizado")
-
-    binary_path = ALLOWED_COMMANDS[command_name]
-
-    try:
-        result = subprocess.run(
-            [binary_path] + params,
-            capture_output=True,
-            text=True,
-            shell=False,
-            timeout=5
+    if command not in ALLOWED_COMMANDS:
+        raise HTTPException(
+            status_code=403,
+            detail="Comando no autorizado"
         )
 
-        return result
+    binary_path = ALLOWED_COMMANDS[command]
 
-    except subprocess.TimeoutExpired:
-        raise HTTPException(status_code=504, detail="Tiempo límite excedido")
+    result = subprocess.run(
+        [binary_path] + params,
+        capture_output=True,
+        text=True,
+        shell=False,
+        timeout=5
+    )
+
+    return result
