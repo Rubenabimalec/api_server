3f8828811a728e1ae06a56471b3d46c84ab6123e
rubenabimalec <al19760767@ite.edu.mx>
2026-03-06 15:16:33 -0800

    no hay modificaciones

--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 from routes.system import router as system_router
 
-app = FastAPI(title="Linux Command Executor API")
+app = FastAPI()
 
 app.include_router(system_router)
 
 ##EJECUTA EL SIGUIENTE COMANDO DESDE OTRA CONSOLA:
 # CADA QUE MODIFICAS CODIGO RECARGAR EL SERVER: uvicorn main:app --reload
 ##FORMA CORRECTA DE EJECUTAR COMOANDO DESDE OTRA CONSOLA:
-#curl -X POST "http://127.0.0.1:8000/system/execute?command_name=uptime"   -u "caballero:4220"   -H "Content-Type: application/json"   -d '[]'
+#curl -X POST http://localhost:8000/system/execute \                                                   
+#-u caballero:Password \
+#-H "Content-Type: application/json" \
+#-d '{
+#  "command": "df",
+#  "params": ["-h"]
+# }'
